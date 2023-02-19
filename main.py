70080eb3de0039c311ad3d39fb25cd250f9e4ea6
olegrix2222 <125788873+olegrix2222@users.noreply.github.com>
2023-02-19 08:58:46 +0000

    Error code

--- main.py
+++ main.py
@@ -4,14 +4,28 @@ class Person:
   name = "Oleh"
   weight = 43
   is_male = True
-
+  hobby = "ice skiing"
+  
   def __init__(self, surname):
     self.surname = surname
 
 
 me = Person("Ignatyhc:")
-
+you = Person("Test")
 print(me.age)
 # print(me.surname)
 
 
+def do_my_thing(self):
+  print("I love", self.hobby)
+
+  
+  # make class
+me = Person("Ignatyhc:")
+
+hobby = "ice skiing"
+me.do_my_thing()
+
+my_friend = Person("Zahar")
+
+my_friend.hobby = "Play roblox"
